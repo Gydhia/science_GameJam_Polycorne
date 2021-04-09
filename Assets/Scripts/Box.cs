c54b83ab4555cb16367875679efe9a7809a4a682
Aps74 <jeremie@polycorne.fr>
2021-04-10 00:26:26 +0200

    jeremie: first classes

---
+++ Assets/Scripts/Box.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteAlways]
+    public class Box : MonoBehaviour
+    {
+        public BoxSO BoxSO;
+
+        /// <summary>
+        /// List of space available for cards
+        /// </summary>
+        public CardSpace[] CardSpaces;
+
+        /// <summary>
+        /// First half on left
+        /// Second half on right
+        /// </summary>
+        public Hand[] HandsLeft;
+        public Hand[] HandsRight;
+        public GameObject HandPrefab;
+
+        public void Start()
+        {
+            if (this.BoxSO == null)
+                throw new Exception("BoxSO is not defined for this Box");
+
+            //cards init
+            this.CardSpaces = new CardSpace[this.BoxSO.TrackLenght * this.BoxSO.TrackWidth];
+            for(int i = 0; i < this.BoxSO.Cards.Length ; i++)
+            {
+                this.CardSpaces[i] = new CardSpace();
+                this.CardSpaces[i].Card = new Card(this.BoxSO.Cards[i]);
+            }
+
+            //hands init
+            this.HandsLeft = new Hand[this.BoxSO.TrackWidth];
+            this.HandsRight = new Hand[this.BoxSO.TrackWidth];
+
+            float height = this.transform.localScale.y;
+            float width = this.transform.localScale.x;
+            for (int j = 0; j < this.HandsLeft.Length; j++)
+            {
+                GameObject newHand = GameObject.Instantiate(this.HandPrefab, this.transform);
+                newHand.transform.localPosition = new Vector3(0, (height / this.HandsLeft.Length) * j, 0);
+            }
+
+            for (int j = 0; j < this.HandsRight.Length; j++)
+            {
+                GameObject newHand = GameObject.Instantiate(this.HandPrefab, this.transform);
+                newHand.transform.localPosition = new Vector3(width, (height / this.HandsLeft.Length) * j, 0);
+            }
+
+            this.gameObject.transform.localScale = new Vector3(this.BoxSO.TrackLenght, this.BoxSO.TrackWidth);
+        }
+    }
+}
---
+++ Assets/Scripts/BoxSO.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    [ExecuteAlways]
+    [CreateAssetMenu(menuName = "Box")]
+    public class BoxSO : ScriptableObject
+    {
+        public int TrackLenght;
+        public int TrackWidth;
+        public CardSO[] Cards;
+
+        public void Awake()
+        {
+            this.Cards = new CardSO[this.TrackLenght * this.TrackWidth];
+        }
+    }
+}
