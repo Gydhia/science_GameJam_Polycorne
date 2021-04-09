12287fe95378616fe58ea0e306f105f683543d32
Aurel Jacquot <aurel@polycorne.fr>
2021-04-10 00:27:08 +0200

    aurel: added distribution for trainstation

--- Assets/Scripts/TrainStation.cs
+++ Assets/Scripts/TrainStation.cs
@@ -9,14 +9,40 @@ public class TrainStation : MonoBehaviour
 {
     public List<LineRenderer> Outputs;
     public List<double> OutputDistribution;
+    public System.Random rand;
 
 
+    public void Start()
+    {
+        this.rand = new System.Random();
+    }
 
     public void SendTrain()
     {
         var train = GameObject.Instantiate<Train>(Resources.Load<Train>("prefabs/train"));
-        train.PlaceOnTracks(this.Outputs.First());
+        int trackID = this.DecideOutput();
+        train.PlaceOnTracks(this.Outputs.ElementAt(trackID));
     }
 
+    private int DecideOutput()
+    {
+        if (this.OutputDistribution == null || this.OutputDistribution.Count() <= 1)
+            // if no distribution was set, no weights: return a random output
+            return this.rand.Next(0, this.Outputs.Count());
 
+        double total_weight = this.OutputDistribution.Sum();
+        // if no weight was set, no weights: return a random output
+        if (total_weight == 0)
+            return this.rand.Next(0, this.Outputs.Count());
+
+        var result = this.rand.NextDouble() * total_weight;
+        
+        int chosen_output = -1;
+        while (result > 0)
+        {
+            chosen_output++;
+            result -= this.OutputDistribution.ElementAt(chosen_output);
+        }
+        return chosen_output;
+    }
 }
