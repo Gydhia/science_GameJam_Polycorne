8100435d4ba60e3ca3caa8d1acd9753cd4524b16
Jimmy <jimmy@polycorne.fr>
2021-04-10 00:17:08 +0200

    GJ-001: LineRenderer drawing function

---
+++ Assets/Scripts/Cards.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cards : MonoBehaviour
+{
+    public void connectLine(Vector3 start, Vector3 end)
+    {
+        GameObject line = new GameObject();
+        line.transform.position = start;
+        line.AddComponent<LineRenderer>();
+
+        LineRenderer line_renderer = line.GetComponent<LineRenderer>();
+
+        line_renderer.SetPosition(0, start);
+        line_renderer.SetPosition(1, end);
+    }
+}
