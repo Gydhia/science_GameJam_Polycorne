f0627652c5fdaf0e8c414ed6005f305bf66de47c
Aps74 <jeremie@polycorne.fr>
2021-04-10 14:54:28 +0200

    jeremie: added regenerate functions

---
+++ Assets/Scripts/Editor/BaseUIPanelInspector.cs
@@ -0,0 +1,36 @@
+﻿using Assets.Scripts;
+using UnityEditor;
+using UnityEngine;
+
+namespace ScienceGameJam.UnityEditor
+{
+   
+    [CustomEditor(typeof(Box))]
+    public class BaseUIPanelInspector : Editor
+    {
+        Box target;
+
+        public void OnEnable()
+        {
+            if(base.target != null)
+                this.target = (Box)base.target;
+        }
+        
+
+        public override void OnInspectorGUI()
+        {
+            if (GUI.Button(GUILayoutUtility.GetRect(0, int.MaxValue, 20, 20), "Regenerate Cards Space"))
+            {
+                this.target.RegenerateCardsspace();
+            }
+
+            if (GUI.Button(GUILayoutUtility.GetRect(0, int.MaxValue, 20, 20), "Regenerate Hands"))
+            {
+                this.target.RegenerateHands();
+            }
+
+            base.OnInspectorGUI();
+        }
+    }
+
+}
