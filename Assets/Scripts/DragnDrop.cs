e2268ced2afdf833a769984eb7b787092d898539
Aps74 <jeremie@polycorne.fr>
2021-04-10 14:40:44 +0200

    jeremie: add

--- Assets/Scripts/DragnDrop.cs
+++ Assets/Scripts/DragnDrop.cs
@@ -13,13 +13,14 @@ namespace Assets.Scripts
         private Canvas canvas;
         private RectTransform rectTransform;
         private CanvasGroup canvasGroup;
+        private RectTransform canvasRectTransform;
 
         private void Start()
         {
             rectTransform = GetComponent<RectTransform>();
             canvasGroup = GetComponent<CanvasGroup>();
             canvas = GameObject.FindObjectOfType<Board>().UICanvas;
-            //canvasRectTransform = this.gameObject.GetComponent<RectTransform>();
+            canvasRectTransform = this.gameObject.GetComponent<RectTransform>();
         }
 
         public void OnPointerDown(PointerEventData eventData)
@@ -37,13 +38,37 @@ namespace Assets.Scripts
         {
             //Vector2 delta = ScreenToCanvas(eventData.position) - ScreenToCanvas(eventData.position - eventData.delta);
             //rectTransform.anchoredPosition += delta * canvas.scaleFactor;
-            this.rectTransform.anchoredPosition += eventData.delta / this.canvas.scaleFactor;
+            //this.rectTransform.anchoredPosition += eventData.delta / this.canvas.scaleFactor;
+            this.transform.position = Input.mousePosition;
         }
 
         public void OnEndDrag(PointerEventData eventData)
         {
             canvasGroup.alpha = 1f;
             canvasGroup.blocksRaycasts = true;
+
+            this.rectTransform.anchoredPosition = LimitRectBoundsToCanvas();
+        }
+
+
+
+        /// <summary>
+        /// Return the anchored position of the root element clamped inside the canvas position
+        /// </summary>
+        /// <returns>The anchored clamped position</returns>
+        private Vector3 LimitRectBoundsToCanvas()
+        {
+            Vector3 localPosition = Vector3.zero;
+            Vector2 rectPosition = this.rectTransform.anchoredPosition;
+
+            if (this.canvas.renderMode == RenderMode.ScreenSpaceOverlay || (this.canvas.renderMode == RenderMode.ScreenSpaceCamera && this.canvas.worldCamera == null))
+            {
+                // keep panel inside layout
+                localPosition.x = Mathf.Clamp(rectPosition.x, Vector2.zero.x, this.canvasRectTransform.rect.width - this.rectTransform.rect.width);
+                localPosition.y = Mathf.Clamp(rectPosition.y, -this.canvasRectTransform.rect.height + this.rectTransform.rect.height, Vector2.zero.y);
+            }
+
+            return localPosition;
         }
     }
 }
