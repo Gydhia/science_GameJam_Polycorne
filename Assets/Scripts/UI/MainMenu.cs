44bbeb93f0b7474e21d67dee8fe746d27a7ecdb2
Jimmy <jimmy@polycorne.fr>
2021-04-10 16:20:33 +0200

    GJ-001: Function for MainMenu Button

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public Button PlayButton;
+        public Button CreditsButton;
+        public Button ExitButton;
+
+        void OnClickPlayButton()
+        {
+            SceneManager.LoadScene("SceneDuJeu");
+        }
+
+        public void OnClickExitButton()
+        {
+            print("ouiiiiiiii");
+            Application.Quit();
+        }
+    }
+}
