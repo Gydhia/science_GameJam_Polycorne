edf318c2e5300953ecea4bb18d50faa2b289137c
Jimmy <jimmy@polycorne.fr>
2021-04-11 15:02:36 +0200

    GJ-001: Popup close button

--- Assets/Scripts/UI/GameUI.cs
+++ Assets/Scripts/UI/GameUI.cs
@@ -8,10 +8,18 @@ namespace Assets.Scripts.UI
     public class GameUI : MonoBehaviour
     {
         public PlayableDirector director;
+        public GameObject EndPopup;
+        public GameObject DialogPopup;
 
         public void StartTimeline()
         {
             director.Play();
+        }
+
+        public void OnClickCloseButton()
+        {
+            DialogPopup.SetActive(false);
+            EndPopup.SetActive(false);
         }
     }
 }
